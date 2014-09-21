3ae6a9c45afab8f836f0b5ce87172b43bd3e1b2c
Dominique Schuwey <domi@domidesign.ch>
2014-09-21 21:56:19 +0200

    bugfixing and initializer method

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ $(document).ready(function() {
     var box = $(event.target);
     var col = color(box) || "";
     if (event.shiftKey && col == "") {
-      $(event.target).addClass("blue")
+      $(event.target).addClass("blue");
     } else {
-      $(event.target).addClass("red")
+      $(event.target).addClass("red");
     }
     if (gameLost(box)) {
-      alert("you lost the game!")
+      alert("you lost the game!");
     }
   });
 });
 
 function newGame() {
-  var times = 10
+  var times = 15;
   for (var i = 0; i < times; i++) {
-    var id = Math.floor((Math.random() * 100 + 1))
-    $("#"+id).addClass("red")
+    var id = Math.floor((Math.random() * 100 + 1));
+    $("#"+id).addClass("red");
   };
   for (var i = 0; i < times; i++) {
-    var id = Math.floor((Math.random() * 100 + 1))
-    $("#"+id).addClass("blue")
+    var id = Math.floor((Math.random() * 100 + 1));
+    $("#"+id).addClass("blue");
   };
 }
 
 // gest the color of the passed div, only works if the div has 2 classes
 function color(div) {
-  var div = div
+  var div = div;
   var color = div.attr("class").split(" ")[1];
   return color;
 }
@@ -45,18 +45,18 @@ function gameLost(box) {
   } 
   // check there are 3 following colors in the row
   var id = parseInt($(box).attr("id"));
-  var nextBox = $("#"+(id+10))
-  var nextNextBox = $("#"+(id+20))
-  var prevBox = $("#"+(id-10))
-  var prevPrevBox = $("#"+(id-20))
+  var nextBox = $("#"+(id+10));
+  var nextNextBox = $("#"+(id+20));
+  var prevBox = $("#"+(id-10));
+  var prevPrevBox = $("#"+(id-20));
   if (color(box) == color(nextBox) &&
       color(box) == color(prevBox) ||
       color(box) == color(nextBox) &&
       color(box) == color(nextNextBox) ||
       color(box) == color(prevBox) &&
       color(box) == color(prevPrevBox)) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   };
 }
